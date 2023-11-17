d44951e9b4ff78b2683d19adef1f1f1f4a610156
Hakase <hakase@litdevs.org>
2023-11-17 23:40:17 +0100

    use postAuthor when available

--- src/components/telegram/dialogs/TelegramMessage.jsx
+++ src/components/telegram/dialogs/TelegramMessage.jsx
@@ -2,8 +2,9 @@ import TelegramProfilePicture from "../TelegramProfilePicture.jsx";
 import styles from "./TelegramMessage.module.css"
 
 export default function TelegramMessage({message}) {
+    console.log(message)
     return (<div className={styles.message}>
         <TelegramProfilePicture photo={message._sender.photo} peer={message._inputSender}/>
-        <span className={styles.content}> <b>{message._sender.firstName}</b><br/>{message.message}</span>
+        <span className={styles.content}> <b>{message._sender.firstName || message.postAuthor}</b><br/>{message.message}</span>
     </div>)
 }
