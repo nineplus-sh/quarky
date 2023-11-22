681a16c5824fab910038a5c51cbd5fb30f05237c
Hakase <hakase@litdevs.org>
2023-11-22 01:24:20 +0100

    Add fallbacks for action and media messages

--- src/components/telegram/dialogs/TelegramMessage.jsx
+++ src/components/telegram/dialogs/TelegramMessage.jsx
@@ -2,8 +2,15 @@ import TelegramProfilePicture from "../TelegramProfilePicture.jsx";
 import styles from "./TelegramMessage.module.css"
 
 export default function TelegramMessage({message}) {
-    return (<div className={styles.message}>
-        <TelegramProfilePicture photo={message._sender.photo} peer={message._inputSender}/>
-        <span className={styles.content}> <b>{message._sender.firstName || message.postAuthor}</b><br/>{message.message}</span>
-    </div>)
+    console.log(message._sender, message)
+    if(message.action || message.media) {
+        return (<div className={styles.message}>
+            <i>This version of Quarky does not support this message.</i>
+        </div>)
+    } else {
+        return (<div className={styles.message}>
+            <TelegramProfilePicture photo={message._sender.photo} peer={message._inputSender}/>
+            <span className={styles.content}> <b>{message._sender.firstName || message.postAuthor}</b><br/>{message.message}</span>
+        </div>)
+    }
 }
