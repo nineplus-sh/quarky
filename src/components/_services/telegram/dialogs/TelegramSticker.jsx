c90c74235d4264908e9948d01704cc0dd63b8fb6
Hakase <hakase@litdevs.org>
2024-03-20 19:58:38 +0100

    Lottie sticker rendering

--- src/components/_services/telegram/dialogs/TelegramSticker.jsx
+++ src/components/_services/telegram/dialogs/TelegramSticker.jsx
@@ -3,14 +3,14 @@ import pathToSvg from "../../../../util/pathToSvg.js";
 import svgToImgURL from "../../../../util/svgToImgURL.js";
 import {Api} from "telegram";
 import {AppContext} from "../../../../contexts/AppContext.js";
+import Lottie from 'react-lottie';
+import * as pako from "pako";
 
 export default function TelegramSticker({media}) {
     const appContext = useContext(AppContext)
 
     useEffect(() => {
         (async () => {
-            if(media.mimeType !== "image/webp") return;
-
             const stickerLocation = new Api.InputDocumentFileLocation({
                 id: media.id.value,
                 fileReference: media.fileReference,
@@ -18,6 +18,7 @@ export default function TelegramSticker({media}) {
                 thumbSize: ""
             })
             const stickerDownload = await appContext.telegram.downloadFile(stickerLocation);
+            if(media.mimeType === "application/x-tgsticker") return setLottie(JSON.parse(pako.inflate(stickerDownload, { to: 'string' })))
             const dataUrl = `data:image/webp;base64,${stickerDownload.toString("base64")}`;
             setImage(dataUrl)
         })()
@@ -26,7 +27,7 @@ export default function TelegramSticker({media}) {
     const photoPath = media.thumbs.find(thumb => thumb.className === "PhotoPathSize");
     const photoSize = media.thumbs.find(thumb => thumb.className === "PhotoSize");
     const [image, setImage] = useState(photoPath ? svgToImgURL(pathToSvg(photoPath.bytes)) : "")
+    const [lottie, setLottie] = useState(undefined)
 
-    if(media.mimeType !== "image/webp") return <i>This version of Quarky does not support this sticker type.</i>
-    return <img src={image} width={photoSize.w} height={photoSize.h} />
+    return lottie ? <Lottie options={{loop: true, autoplay: true, animationData: lottie}} width={256}/> : <img src={image} width={photoSize.w} height={photoSize.h} />
 }
