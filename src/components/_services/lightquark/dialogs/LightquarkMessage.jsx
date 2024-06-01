d11e575e24e61bf798a4e08dc5e6cebbb9e1bf43
Hakase <hakase@litdevs.org>
2024-06-01 21:35:36 +0200

    usercache

--- src/components/_services/lightquark/dialogs/LightquarkMessage.jsx
+++ src/components/_services/lightquark/dialogs/LightquarkMessage.jsx
@@ -1,11 +1,14 @@
 import Message from "../../../dialogs/Message.jsx";
 import ProfilePicture from "../../../ProfilePicture.jsx";
+import {useContext} from "react";
+import {AppContext} from "../../../../contexts/AppContext.js";
 
 export default function LightquarkMessage({message}) {
     const botMetadata = message.specialAttributes.find(attr => attr.type === "botMessage");
+    const {userCache} = useContext(AppContext);
 
-    return <Message username={botMetadata?.username || message.author.username}
-                    avatar={<ProfilePicture src={botMetadata?.avatarUri || message.author.avatarUri} isMessage={true}/>}
+    return <Message username={botMetadata?.username || userCache[message.author._id]?.username || + message.author.username}
+                    avatar={<ProfilePicture src={botMetadata?.avatarUri || userCache[message.author.id]?.avatarUri || message.author.avatarUri} isMessage={true}/>}
                     content={message.content}>
         {message.content}
     </Message>
