7f55c5e26d46fdaaef806abd85ba00fae2638861
Hakase <hakase@litdevs.org>
2024-06-15 22:35:02 +0200

    hide playing activity page on web

--- src/components/nav/SettingsSidebar.jsx
+++ src/components/nav/SettingsSidebar.jsx
@@ -2,6 +2,7 @@ import SidebarItem from "./SidebarItem.jsx";
 import {useTranslation} from "react-i18next";
 import styles from "./SettingsSidebar.module.css";
 import NyafileImage from "../nyafile/NyafileImage.jsx";
+import {version, codename} from "../../../package.json";
 
 export default function SettingsSidebar({area, setArea}) {
     const {t} = useTranslation();
@@ -12,10 +13,23 @@ export default function SettingsSidebar({area, setArea}) {
         </div>
         <SidebarItem baseI18n={"SETTINGS"} area={"profile"} currentArea={area} setArea={setArea}/>
 
+        <div className={styles.separator}/>
+
         <div className={styles.sidebarHeader}>
             <span>{t("SETTINGS_APPSETTINGS")}</span>
         </div>
         <SidebarItem baseI18n={"SETTINGS"} area={"chat"} currentArea={area} setArea={setArea}/>
-        <SidebarItem baseI18n={"SETTINGS"} area={"playing"} currentArea={area} setArea={setArea}/>
+        {window.hiddenside.hardcoreGaming ? <SidebarItem baseI18n={"SETTINGS"} area={"playing"} currentArea={area} setArea={setArea}/> : null }
+
+        <div className={styles.separator}/>
+
+        <div className={styles.version}>
+            <p>{t("QUARKY_VERSION", {version, codename})}</p>
+            <NyafileImage src={"img/nineball"} className={styles.nineplus}/>
+        </div>
+        <div className={styles.sns}>
+            <a href={"https://twitter.com/nineplus_sh"} target={"_blank"} rel="noreferrer"><NyafileImage src={"img/twitter"} inlinesvg={true}/></a>
+            <a href={"https://tumblr.com/nineplus-sh"} target={"_blank"} rel="noreferrer"><NyafileImage src={"img/tumblr"} inlinesvg={true}/></a>
+        </div>
     </div>
 }
