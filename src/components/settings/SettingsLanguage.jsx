db2ed1db6dffda31e4747ac7c8b932c96c85506f
Hakase <hakase@litdevs.org>
2024-06-30 12:49:09 +0200

    add cute engwish to selector

--- src/components/settings/SettingsLanguage.jsx
+++ src/components/settings/SettingsLanguage.jsx
@@ -4,11 +4,18 @@ import locallangs from "../../util/locallangs.json"
 
 export default function SettingsLanguage() {
     const languages = ["en", "fi", "nb_NO", "ja"];
+    const jokeLanguages = ["en_UWU"];
     const {t} = useTranslation();
+
+    function langmap(languages) {
+        return languages.map(language => <SettingsRadio setting={"LANGUAGE"} value={language} key={language} externalIcon={`https://translit.litdevs.org/widget/quarky/quarky/${language}/88x31-grey.png`}
+                                                 text={t("SETTINGS_LANGUAGE_FORMAT", {localname: locallangs[language], translatedname: t(`SETTING_LANGUAGE_${language.toUpperCase()}`)})}/>)
+    }
 
     return <>
         <p><Trans i18nKey={"SETTINGS_LANGUAGE_INVITE"} components={[<a href={"https://translit.litdevs.org/projects/quarky/quarky/"} target={"_blank"} rel={"noreferrer"}/>]}/></p>
-        {languages.map(language => <SettingsRadio setting={"LANGUAGE"} value={language} key={language} externalIcon={`https://translit.litdevs.org/widget/quarky/quarky/${language}/88x31-grey.png`}
-                                                    text={t("SETTINGS_LANGUAGE_FORMAT", {localname: locallangs[language], translatedname: t(`SETTING_LANGUAGE_${language.toUpperCase()}`)})}/>)}
+        {langmap(languages)}
+        <hr/>
+        {langmap(jokeLanguages)}
     </>
 }
