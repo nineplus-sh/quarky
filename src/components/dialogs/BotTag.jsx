4e1ac6b99bd51fbfe195f1347696cc5f56325d0a
Hakase <hakase@litdevs.org>
2024-07-04 10:58:57 +0200

    move bottag into strings

--- src/components/dialogs/BotTag.jsx
+++ src/components/dialogs/BotTag.jsx
@@ -1,9 +1,11 @@
 import styles from "./BotTag.module.css"
 import classnames from "classnames";
+import {useTranslation} from "react-i18next";
 
 export default function BotTag({name, isDiscord}) {
+    const {t} = useTranslation();
 
     return <span className={classnames(styles.botTag, {[styles.discord]: isDiscord})}>
-        {isDiscord ? "DISCORD" : "BOT"}
+        {isDiscord ? t("DISCORD") : t("BOT")}
     </span>
 }
