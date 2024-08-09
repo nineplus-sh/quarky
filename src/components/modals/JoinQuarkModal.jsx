50e3cd12e66365d5d924370e985315ee78bd2093
Hakase <hakase@litdevs.org>
2024-08-09 10:42:30 +0200

    create quarks in join modal

--- src/components/modals/JoinQuarkModal.jsx
+++ src/components/modals/JoinQuarkModal.jsx
@@ -14,12 +14,23 @@ export default NiceModal.create(() =>{
     const { t } = useTranslation();
     const [inviteCode, setInviteCode] = useState("");
     const [isJoining, setIsJoining] = useState(false);
+    const [tab, switchTab] = useState("join");
+    const [createName, setCreateName] = useState("");
+    const [createCode, setCreateCode] = useState("");
 
     async function joinQuark(e) {
         e.preventDefault();
         setIsJoining(true);
 
         await LQ(`quark/${inviteCode}/join`, "POST", {"invite": inviteCode});
+        modal.hide();
+    }
+
+    async function createQuark(e) {
+        e.preventDefault();
+        setIsJoining(true);
+
+        await LQ("quark", "POST", {name: createName, code: createCode});
         modal.hide();
     }
 
@@ -30,13 +41,31 @@ export default NiceModal.create(() =>{
                     <NyafileImage src={"img/modal_join_quark"} className={styles.modalImage}/>
                 </div>
                 <div>
-                    <h2 style={{margin: 0}}>{t("JOIN_QUARK")}</h2>
-                    <p style={{marginTop: 0}}>{t("JOIN_QUARK_BODY")}</p>
+                    {tab === "join" ? <>
+                        <h2 style={{margin: 0}}>{t("JOIN_QUARK")}</h2>
+                        <p style={{marginTop: 0}}>{t("JOIN_QUARK_BODY")}</p>
 
-                    <form onSubmit={(e) => joinQuark(e)}><fieldset disabled={isJoining}>
-                            <input type={"text"} required placeholder={"litdevs"} value={inviteCode} onChange={e => setInviteCode(e.target.value)}/>
-                            <input type={"submit"}/>
-                    </fieldset></form>
+                        <form onSubmit={(e) => joinQuark(e)}><fieldset disabled={isJoining}>
+                                <input type={"text"} required placeholder={"litdevs"} value={inviteCode} onChange={e => setInviteCode(e.target.value)}/>
+                                <input type={"submit"}/>
+                        </fieldset></form>
+
+                        <p style={{marginBottom: 0}}>or <button onClick={() => switchTab("create")}>create a quark</button></p>
+                    </> : tab === "create" ? <>
+                        <h2 style={{marginTop: 0}}>Create a quark!</h2>
+
+                        <form onSubmit={(e) => createQuark(e)}>
+                            <fieldset disabled={isJoining}>
+                                <input type={"text"} required placeholder={"Quark name"} value={createName}
+                                       onChange={e => setCreateName(e.target.value)}/>
+                                <input type={"text"} required placeholder={"Invite code"} value={createCode}
+                                       onChange={e => setCreateCode(e.target.value)}/>
+                                <input type={"submit"}/>
+                            </fieldset>
+                        </form>
+
+                        <p style={{marginBottom: 0}}>or <button onClick={() => switchTab("join")}>join a quark</button></p>
+                    </> : <p>Something went wrong.</p>}
                 </div>
             </div>
         </GenericModal>
