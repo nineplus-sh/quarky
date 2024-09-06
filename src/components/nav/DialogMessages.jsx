65db744b67c04b52ffa73564735603da0d5c03d4
Hakase <hakase@litdevs.org>
2024-09-06 22:16:07 +0200

    decouple scrollintoview

--- src/components/nav/DialogMessages.jsx
+++ src/components/nav/DialogMessages.jsx
@@ -1,4 +1,4 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useLayoutEffect, useState} from "react";
 import {AppContext} from "../../contexts/AppContext.js";
 import {useParams} from "react-router-dom";
 import LightquarkMessage from "../_services/lightquark/dialogs/LightquarkMessage.jsx";
@@ -26,9 +26,10 @@ export default function DialogMessages() {
     useEffect(() => {
         if(!appContext.messageCache[dialogId]) return setMessages([]);
         setMessages(appContext.messageCache[dialogId].sort((a, b) => a.timestamp - b.timestamp))
-
-        document.querySelector("div[class^='_messages_']").lastChild?.scrollIntoView({"behavior": "smooth"});
     }, [dialogId, appContext.messageCache])
+    useLayoutEffect(() => {
+        document.querySelector("div[class^='_messages_']").lastChild?.scrollIntoView({"behavior": "smooth"});
+    }, [messages]);
 
     return messages.map((message, index) => {
         let sameAuthor = false;
