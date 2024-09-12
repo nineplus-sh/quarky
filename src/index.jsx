6e341957ccb744a53bccd453873efa22e4df098d
Hakase <hakase@litdevs.org>
2024-09-12 08:32:51 +0200

    fumbling around on mobile

--- src/index.jsx
+++ src/index.jsx
@@ -51,9 +51,11 @@ Sentry.init({
         new Sentry.Replay({
             useCompression: false
         }),
+        Sentry.browserProfilingIntegration()
     ],
     // Performance Monitoring
     tracesSampleRate: 1.0, // Capture 100% of the transactions
+    profilesSampleRate: 1.0,
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
