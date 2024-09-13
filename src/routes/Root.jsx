620c9c8434ffd3ce4a91fdecd347eab6b8c02497
Hakase <hakase@litdevs.org>
2024-09-13 21:05:56 +0200

    No more infinite pippispin dot nineplus dot sh, this is quarky

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -98,7 +98,7 @@ export default function Root() {
                 })
             }
 
-            //appContext.setLoading(false);
+            appContext.setLoading(false);
         }
         loadNyafile();
     }, []);
