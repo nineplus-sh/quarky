5f1b51653e85f1aaea5e53be5e752be2f27ce0f6
Hakase <hakase@litdevs.org>
2024-09-14 13:09:30 +0200

    add hp

--- src/routes/Loader.jsx
+++ src/routes/Loader.jsx
@@ -1,10 +1,11 @@
 import styles from "./Loader.module.css"
 import {version, author} from "../../package.json"
 import {useTranslation} from "react-i18next";
-import {useContext, useEffect, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {AppContext} from "../contexts/AppContext.js";
 import NyafileImage from "../components/nyafile/NyafileImage.jsx";
 import classnames from "classnames";
+import seededRandom from "../util/seededRandom.js";
 
 /**
  * The loading screen.
@@ -13,31 +14,85 @@ import classnames from "classnames";
  */
 export default function Loader({loadingString, progress, progressString}) {
     const { t } = useTranslation();
-    const { nyafile, holiday } = useContext(AppContext);
+    const { holiday } = useContext(AppContext);
     const [pippiDiedBigSadFrownyFace, setPippiDiedBigSadFrownyFace] = useState(false);
+    const [stabbed, setStabbed] = useState(false);
+    const [splats, setSplats] = useState([]);
+    const quarkyLogo = useRef(null);
+    const [HP, setHP] = useState(10);
 
-    return (<div id="superpreload">
-        <img src="/quarky.svg" id="preloadlogo"/>
-        <span id="preloadtext">
+    function eyeMover(event) {
+        if(!quarkyLogo.current) return;
+        const rect = quarkyLogo.current.getBoundingClientRect();
+        const rectX = rect.left + (rect.width / 2);
+        const rectY = rect.top + (rect.height / 2);
+        const calcX = (event.clientX - rectX) / 80;
+        const calcY = (event.clientY - rectY) / 80;
+
+        quarkyLogo.current.contentDocument.querySelector("#eyelayer").style.transform = `
+            translateX(${Math.max(-7, Math.min(7, calcX))}px) 
+            translateY(${Math.max(-5, Math.min(5, calcY))}px)
+        `
+    }
+    function owie(event) {
+        if(HP > 0) {
+            setHP(HP => HP - 1)
+            if(HP === 1) new Audio("/explode.mp3").play();
+        } else {
+            return;
+        }
+
+        const splatID = Date.now();
+        setSplats(oldSplats => [...oldSplats,{seed: splatID, clientX: event.clientX, clientY: event.clientY}] );
+        setTimeout(() => setSplats(oldSplats => oldSplats.filter(splat => splat.seed !== splatID)), 1000)
+
+        setStabbed(false);
+        setTimeout(() => setStabbed(true), 10);
+    }
+    useEffect(() => {
+        document.addEventListener("mousemove", eyeMover);
+        return () => document.removeEventListener("mousemove", eyeMover);
+    }, []);
+
+    return (<><div className={styles.hitsplats}>{splats.map(splat => <Hitsplat data={splat} key={splat.seed}/>)}</div>
+        <div id="superpreload">
+            <div className={classnames(styles.logoEffectWrap, {[styles.stabbed]: stabbed && HP > 0})} onClick={owie}>
+                {HP === 0 ? <img src={"/explode.gif"} height={160} id="preloadlogo"/> : <object data={"/quarky.svg"} id="preloadlogo" ref={quarkyLogo}/>}
+            </div>
+
+            <span id="preloadtext">
             {t(loadingString)}
-            <br/>
+                <br/>
             <i>{t(`${holiday}_LINK`) !== `${holiday}_LINK` ?
                 <a target={"_blank"} rel={"noreferrer noopener"} href={t(`${holiday}_LINK`)}>{t(holiday)}</a>
                 : t(holiday)}</i>
-            {progress ? <><br/>
-                <div className={classnames(styles.loadingBarWrapper, {[styles.pippiless]: pippiDiedBigSadFrownyFace})}>
-                    <img className={styles.loadingBarImage} style={{left: `${progress}%`}}
-                         src={"/loading.gif"}/>
+                {progress ? <><br/>
+                    <div
+                        className={classnames(styles.loadingBarWrapper, {[styles.pippiless]: pippiDiedBigSadFrownyFace})}>
+                        <img className={styles.loadingBarImage} style={{left: `${progress}%`}}
+                             src={"/loading.gif"}/>
 
-                    <div className={styles.loadingBar} onMouseOver={() => setPippiDiedBigSadFrownyFace(true)}>
-                        <div className={styles.loadingBarStretcher} style={{width: `${progress}%`}}></div>
-                    </div>
+                        <div className={styles.loadingBar} onMouseOver={() => {
+                            if(pippiDiedBigSadFrownyFace) return;
+                            setPippiDiedBigSadFrownyFace(true)
+                            new Audio("/fall.mp3").play()
+                        }}>
+                            <div className={styles.loadingBarStretcher} style={{width: `${progress}%`}}></div>
+                        </div>
 
-                    <div className={styles.loadingBarText}>
-                        {progressString}
+                        <div className={styles.loadingBarText}>
+                            {progressString}
+                        </div>
                     </div>
-                </div>
-            </> : null}
+                </> : null}
         </span>
-    </div>)
+    </div></>)
+}
+
+export function Hitsplat({data}) {
+    return <img className={styles.hitsplat} style={{
+        position: "fixed",
+        left: data.clientX,
+        top: data.clientY
+    }} src="/hitsplat.png"/>
 }
